cbe3f4dc66e3328dd76bf58ddb212237f5dd6c8b
Snehal559 <snehalvingole@gmail.com>
2024-07-08 09:56:09 +0530

    Update Demo.java

--- GithubProject/src/com/cjc/Demo.java
+++ GithubProject/src/com/cjc/Demo.java
@@ -3,6 +3,14 @@ package com.cjc;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("heloo world");
+		Demo d=new Demo();
+		d.m1();
 	}
+
+	public void m1()
+	{
+				System.out.println("m1 method");
+	}
+
 
 }
